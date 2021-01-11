f9e044c981bb00c61cb43422aab57cf665790975
tunchin kao <tunchinkao@gmail.com>
2021-01-11 23:57:42 +0800

    updata .gitignore

--- sources/verilog_files/scene/start_scene.v
+++ sources/verilog_files/scene/start_scene.v
@@ -24,28 +24,20 @@ module start_scene(
         input clk,
         input [9:0]v_cnt,
         input [9:0]h_cnt,
-        output [11:0] vga_data
+        input [11:0] mem_title_vga_data,
+        output [11:0] vga_data,
+        output [16:0] pixel_addr
     );
 parameter title_width = 320;
 parameter title_height = 240;
-    wire [16:0] pixel_addr;
+    // wire [16:0] pixel_addr;
     wire clk_25MHz;
-    wire [11:0] data;
+    // wire [11:0] data;
     assign pixel_addr = ((h_cnt>>1) + title_width * (v_cnt >> 1)) % (title_width * title_height);
     // always @(*) begin
     //     vga_data = 12'h00d;
     // end
-    clock_divisor beta_clk_wiz_0_inst(
-      .clk(clk),
-      .clk1(clk_25MHz)
-    );
-    title_320_240_mem start_scene_in00st(
-      .clka(clk_25MHz),
-      .wea(0),
-      .addra(pixel_addr),
-      .dina(data[11:0]),
-      .douta(vga_data)
-    ); 
+    
 endmodule
 
 // module clock_divisor(clk1, clk, clk22);
